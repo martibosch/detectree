8ed8245b1f1a03d5d14c9ac6417e0191bc74656a
Martí Bosch <marti.bosch@epfl.ch>
2019-09-04 20:33:35 +0200

    tests for image descriptor

--- tests/test_detectree.py
+++ tests/test_detectree.py
@@ -2,6 +2,7 @@ import glob
 import unittest
 from os import path
 
+import numpy as np
 import pandas as pd
 
 import detectree as dtr
@@ -93,3 +94,45 @@ class TestTrainTestSplit(unittest.TestCase):
             split_df, evr = ts.train_test_split(return_evr=True)
             self.assertIsInstance(split_df, pd.DataFrame)
             self.assertIsInstance(evr, float)
+
+
+class TestImageDescriptor(unittest.TestCase):
+    def setUp(self):
+        self.tile_dir = 'tests/data'
+        self.tile_filepath = glob.glob(path.join(self.tile_dir, '*.tif'))[0]
+
+    def test_image_descriptor(self):
+        img_descr = dtr.ImageDescriptor(self.tile_filepath)
+        self.assertIsInstance(img_descr.img_rgb, np.ndarray)
+
+        gabor_frequencies = (.1, .25, .4)
+        gabor_num_orientations = (4, 8, 8)
+        kernels = dtr.get_gabor_filter_bank(
+            frequencies=gabor_frequencies,
+            num_orientations=gabor_num_orientations)
+        response_bins_per_axis = 4
+        num_blocks = response_bins_per_axis**2
+        gist_descr_row = img_descr.get_gist_descriptor(kernels,
+                                                       response_bins_per_axis,
+                                                       num_blocks)
+        self.assertEqual(len(gist_descr_row), len(kernels) * num_blocks)
+
+        # TODO: more technical test, e.g., passing an all-zero filter bank
+        # should return an all-zero gist descriptor
+
+        num_color_bins = 8
+        color_descr_row = img_descr.get_color_descriptor(num_color_bins)
+        self.assertEqual(len(color_descr_row), num_color_bins**3)
+
+        # TODO: more technical test, e.g., ensure that a negative number of
+        # bins raises some NumPy error
+
+        img_descr_row = img_descr.get_image_descriptor(kernels,
+                                                       response_bins_per_axis,
+                                                       num_blocks,
+                                                       num_color_bins)
+        self.assertEqual(len(img_descr_row),
+                         len(gist_descr_row) + len(color_descr_row))
+
+        # TODO: more technical test, e.g., ensure that all values in
+        # `img_descr_row` are within the unit norm
