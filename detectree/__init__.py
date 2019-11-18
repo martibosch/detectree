2e2adbbe6e99193c7cb77f05ebfe03905a235224
Martí Bosch <marti.bosch@epfl.ch>
2019-11-18 09:23:23 +0100

    Bump version: 0.1.0 → 0.1.1

--- detectree/__init__.py
+++ detectree/__init__.py
@@ -2,4 +2,4 @@ from .classifier import Classifier, ClassifierTrainer
 from .train_test_split import TrainingSelector
 from .utils import split_into_tiles
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
