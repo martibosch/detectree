9d20e829cc33bb9418234514e27f20c3767c7438
Martí Bosch <marti.bosch@epfl.ch>
2021-07-05 11:48:20 +0200

    Bump version: 0.4.0 → 0.4.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from os import path
 
 from setuptools import find_packages, setup
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 
 classifiers = [
     "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
