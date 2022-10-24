927e5e914fe2465d82dc4aad8089845c16322589
Martí Bosch <marti.bosch@protonmail.com>
2022-10-24 10:12:28 +0200

    fix: updated Python version matrix (drop 3.6, add 3.10)

--- setup.py
+++ setup.py
@@ -15,8 +15,10 @@ classifiers = [
     "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
     "Programming Language :: Python",
     "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
 ]
 
 here = path.abspath(path.dirname(__file__))
