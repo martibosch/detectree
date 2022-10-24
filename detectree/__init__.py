04ac841fe79478f708dba0a67efd9106258d57e0
Martí Bosch <marti.bosch@protonmail.com>
2022-10-24 13:06:23 +0200

    Bump version: 0.4.1 → 0.4.2

--- detectree/__init__.py
+++ detectree/__init__.py
@@ -4,4 +4,4 @@ from .lidar import LidarToCanopy, rasterize_lidar
 from .train_test_split import TrainingSelector
 from .utils import split_into_tiles
 
-__version__ = "0.4.1"
+__version__ = "0.4.2"
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from os import path
 
 from setuptools import find_packages, setup
 
-__version__ = "0.4.1"
+__version__ = "0.4.2"
 
 classifiers = [
     "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
